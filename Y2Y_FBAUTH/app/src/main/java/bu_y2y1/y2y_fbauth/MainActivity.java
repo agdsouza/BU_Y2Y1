87132488e124ac834ceb022b1539ec9400545b01
rijaz <redaij@bu.edu>
2018-04-09 16:03:20 -0400

    FBAUTH fix

--- Y2Y_FBAUTH/app/src/main/java/bu_y2y1/y2y_fbauth/MainActivity.java
+++ Y2Y_FBAUTH/app/src/main/java/bu_y2y1/y2y_fbauth/MainActivity.java
@@ -3,6 +3,7 @@ package bu_y2y1.y2y_fbauth;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,9 +11,14 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
-import com.facebook.login.LoginManager;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,7 +41,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         info = (TextView)findViewById(R.id.info);
         loginButton = (LoginButton)findViewById(R.id.login_button);
-
+        
 
         //Callback to handle results of the login and register it with Callback Manager
         //Implements the FacebookCallback interface
@@ -43,13 +49,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(LoginResult loginResult) {
                 Toast.makeText(getApplicationContext(), "Success login", Toast.LENGTH_LONG).show();
-                info.setText(
-                        "User ID: "
-                                + loginResult.getAccessToken().getUserId()
-                                + "\n" +
-                                "Auth Token: "
-                                + loginResult.getAccessToken().getToken()
-                );
+
             }
 
             @Override
